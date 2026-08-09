261bd10cb01a4bb57284e68004aa81a521a7faed
Tayyaba Zafar <tayyabazafar1200@gmail.com>
2026-08-09 11:07:07 +0500

    Improve Project 1 code structure
    Cleaned the Student Information System code and improved readability with comments and organized sections.

--- project1_student_information_system.py
+++ project1_student_information_system.py
@@ -1,11 +1,14 @@
 # Project 1 - Student Information System
-print("Student Information System ")
+print("-- Student Information System --")
+# Get student information from the user
 name = input("Enter your Name: ")
 age = input("Enter your Age: ")
 city = input("Enter your City: ")
 cgpa = input("Enter your CGPA: ")
+# Student status
 is_student = True
-print("\n Student Profile")
+# Display student profile
+print("\n-- Student Profile --")
 print("Name:", name)
 print("Age:", age)
 print("City:", city)
